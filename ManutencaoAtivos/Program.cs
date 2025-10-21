fc98637e19a799f0aed9b51da186f4695385bc35
Bruna Ferreira Dadona <bruna.dadona@hotmail.com>
2025-10-21 10:33:11 -0300

    complemento da primeira parte
    adicionado pasta DATA
    alterado algumas coisas no Program.cs
    e adic no appsenttings

--- ManutencaoAtivos/Program.cs
+++ ManutencaoAtivos/Program.cs
@@ -1,6 +1,24 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.EntityFrameworkCore;
+using ManutencaoAtivos.Data;
+
+var builder = WebApplication.CreateBuilder(args)
+
+builder.Services.AppDbContext<AppDbContext>(options => options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddControllers();
+builder.Services.AddEndpontsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+if(app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
 
 app.Run();
