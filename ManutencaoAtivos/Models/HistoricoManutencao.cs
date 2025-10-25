e78b39b72ac1ca84945a98c3fc7502c6ec4a4b8a
Gustavo Stacoviaki Grassi <106496948+grassiii@users.noreply.github.com>
2025-10-25 18:56:07 -0300

    Add files via upload

--- ManutencaoAtivos/Models/HistoricoManutencao.cs
+++ ManutencaoAtivos/Models/HistoricoManutencao.cs
@@ -2,23 +2,22 @@ using System;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace ManutencaoAtivos.Model{
+namespace ManutencaoAtivos.Models{
 /*Classe Hist. Manutencao: Id, CaminhoaId,
  Descricao Serviso, Data servico, Custo e responsavel*/
-    public class HistoricoAtivos
+    public class HistoricoManutencao
     {
         public int Id { get; set; }
 
         public int CaminhaoId { get; set; }
 
-        public string DescricaoServico { get; set; }
+        public required string DescricaoServico { get; set; }
 
-        public DateTime DataServiso { get; set; }
+        public DateTime DataServico { get; set; }
 
         public decimal Custo { get; set; }
 
-        public string Responsavel { get; set; }
+        public required string Responsavel { get; set; }
     
     }
 }
-
